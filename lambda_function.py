f17b9d6b1ab09dc5b589ee28a36a1d3ad28f5da7
Suheab1995 <124862610+Suheab1995@users.noreply.github.com>
2024-05-17 03:50:35 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -11,19 +11,27 @@ def lambda_handler(event, context):
         source_bucket = record['s3']['bucket']['name']
         source_key = urllib.parse.unquote_plus(record['s3']['object']['key'], encoding='utf-8')
 
-        # Check if the object has a PNG extension
-        if source_key.lower().endswith('.png'):
-            # Destination bucket and key
-            destination_bucket = "mybucket19962"
-            destination_key = f"png_objects/{source_key.split('/')[-1]}"
+        # Destination bucket and key for bucket2
+        destination_bucket2 = "mybucket19962"
+        destination_key2 = f"png_objects/{source_key.split('/')[-1]}"
 
-            # Copy the object
+        # Copy the object to bucket2 if it's a PNG image
+        if source_key.lower().endswith('.png'):
             s3.copy_object(
-                Bucket=destination_bucket,
-                Key=destination_key,
+                Bucket=destination_bucket2,
+                Key=destination_key2,
                 CopySource={'Bucket': source_bucket, 'Key': source_key}
             )
-            print(f"Object copied from {source_bucket}/{source_key} to {destination_bucket}/{destination_key}")
-        else:
-            # Object is not a PNG image, leave it in source bucket
-            print(f"Non-PNG object: {source_bucket}/{source_key}. Not copied.")
+            print(f"PNG object copied from {source_bucket}/{source_key} to {destination_bucket2}/{destination_key2}")
+
+        # Destination bucket and key for bucket1
+        destination_bucket1 = source_bucket
+        destination_key1 = source_key
+
+        # Copy the object to bucket1 regardless of file type
+        s3.copy_object(
+            Bucket=destination_bucket1,
+            Key=destination_key1,
+            CopySource={'Bucket': source_bucket, 'Key': source_key}
+        )
+        print(f"Object copied to {destination_bucket1}/{destination_key1}")
