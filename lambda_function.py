26f565fd325dc3f596fe6fdb58decf780f1b8c08
Suheab1995 <124862610+Suheab1995@users.noreply.github.com>
2024-05-17 01:22:06 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -7,23 +7,23 @@ print('Loading function')
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    source_bucket = event['Records'][0]['s3']['bucket']['name']
-    source_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    for record in event['Records']:
+        source_bucket = record['s3']['bucket']['name']
+        source_key = urllib.parse.unquote_plus(record['s3']['object']['key'], encoding='utf-8')
 
-    # Check if the object has a PNG extension
-    if source_key.lower().endswith('.png'):
-        # Destination bucket and key
-        destination_bucket = "mybucket19962"
-        destination_key = f"png_objects/{source_key.split('/')[-1]}"
+        # Check if the object has a PNG extension
+        if source_key.lower().endswith('.png'):
+            # Destination bucket and key
+            destination_bucket = "mybucket19962"
+            destination_key = f"png_objects/{source_key.split('/')[-1]}"
 
-        # Copy the object
-        s3.copy_object(
-            Bucket=destination_bucket,
-            Key=destination_key,
-            CopySource={'Bucket': source_bucket, 'Key': source_key}
-        )
-        print(f"Object copied from {source_bucket}/{source_key} to {destination_bucket}/{destination_key}")
-        return "PNG object copied successfully"
-    else:
-        print(f"Ignoring non-PNG object: {source_bucket}/{source_key}")
-        return "Non-PNG object ignored"
+            # Copy the object
+            s3.copy_object(
+                Bucket=destination_bucket,
+                Key=destination_key,
+                CopySource={'Bucket': source_bucket, 'Key': source_key}
+            )
+            print(f"Object copied from {source_bucket}/{source_key} to {destination_bucket}/{destination_key}")
+        else:
+            # Object is not a PNG image, leave it in source bucket
+            print(f"Non-PNG object: {source_bucket}/{source_key}. Not copied.")
